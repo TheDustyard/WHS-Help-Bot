c68cc685d1d8b27d3c30d2e6ccc0b8e27689c062
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-09-14 13:34:19 -0400

    Change from json to toml and expand configuration

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,41 @@
 use serde::{Deserialize};
+use serenity::model::id::{
+    UserId, RoleId
+};
+use std::collections::HashMap;
 
 #[derive(Deserialize, Debug)]
 pub struct StaticConfiguration {
-    server_id: String;
+    /// Configuration for the server
+    pub server: ServerConfig,
+    /// Configuration for the different roles
+    pub roles: RolesConfig,
+    /// Configuration for the different classes and their levels
+    pub classes: HashMap<String, HashMap<String, ClassConfig>>
 }
 
-#[derive(Deserialize)]
-pub struct SecretConfiguration {
-    token: String;
+#[derive(Deserialize, Debug)]
+pub struct ServerConfig {
+    /// The server id to manage
+    pub id: String,
+    /// The owner of the bot
+    pub owner: UserId,
+    /// The adminatrator roles that can run admin commands
+    pub admin_roles: Vec<RoleId>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RolesConfig {
+    /// Roles to auto assign on join
+    pub auto_assign: Vec<RoleId>,
+    /// The role to designate that a user has joined the server
+    pub joined: Vec<RoleId>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ClassConfig {
+    /// The role that denotes the possesion of the class
+    role: RoleId,
+    /// The name of the class
+    name: String
 }
